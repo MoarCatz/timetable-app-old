f0f5674fb71c6a45579862d8e218fa9282dfe382
Lev <leva181777@gmail.com>
2017-04-30 19:13:53 +0000

    :lipstick: Closes the SideNav before navigating to a different page

--- www/js/common.js
+++ www/js/common.js
@@ -1,9 +1,10 @@
 $(function() {
   $('.button-collapse').sideNav({
-    draggable: true
+    draggable: true,
+    closeOnClick: true
   });
 
   $('.button-collapse').click(function() {
     $('a.dropdown-button.active').click();
-  })
+  });
 });
