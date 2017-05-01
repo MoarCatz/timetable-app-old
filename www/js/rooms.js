bd843fba3edccaa50c79d25bc4ee29fde21731f8
Lev <leva181777@gmail.com>
2017-05-01 17:58:34 +0000

    :zap::lipstick: Removes modal animations, adds a checkbox to set a rule

--- www/js/rooms.js
+++ www/js/rooms.js
@@ -2,8 +2,19 @@ $(document).ready(function(){
   $('.modal').modal();
 });
 
+function showModal(modal) {
+  modal.addClass('open');
+  modal.css({'display': 'block', 'opacity': '1', 'transform': 'scaleX(1)', 'top': '10%'});
+}
+
+function hideModal(modal) {
+  modal.removeClass('open');
+  modal.css({'display': 'none', 'opacity': '0', 'transform': 'scaleX(0.7)', 'top': '4%'});
+}
+
 var $occupied = $('#occupied');
 var $occupy = $('#occupy');
+
 $('.chip').click(function() {
   if ($(this).hasClass('taken')) {
     $occupied.find('h5').text($(this).text());
@@ -11,11 +22,19 @@ $('.chip').click(function() {
     $occupied.find('#reason').text('Спецкурс по математике с Масленниковой М.И.');
     $occupied.find('#until').text('С 6 по 7 урок');
 
-    $occupied.modal('open');
+    showModal($occupied);
   }
   else {
     $occupy.find('h5').text($(this).text());
 
-    $occupy.modal('open');
+    showModal($occupy);
   }
+});
+
+$occupied.find('.modal-close').click(function() {
+  hideModal($occupied);
+});
+
+$occupy.find('.modal-close').click(function() {
+  hideModal($occupy);
 });
