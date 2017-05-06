149f07e6cc82d8f896d5d53cab89aa08bdb5f977
Lev <leva181777@gmail.com>
2017-05-06 13:02:05 +0000

    :fire: Removes old files to get a fresh start

--- www/js/classes.js
+++
@@ -1,8 +0,0 @@
-$('.collapsible-header').click(function(event) {
-  event.stopPropagation();
-  $(this).parents('.collapsible').collapsible('open', $(this).parent().index());
-});
-
-$('li a.btn-flat').click(function() {
-  $('a.dropdown-button.change-text').text($(this).text());
-});
