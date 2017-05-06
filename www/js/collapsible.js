16dbd83d58983d7d9c256439b2e9606ed2f216d4
Lev <leva181777@gmail.com>
2017-05-06 17:58:32 +0000

    :bug: Turns Collapsibles into accordions

--- www/js/collapsible.js
+++ www/js/collapsible.js
@@ -1,9 +1,16 @@
 document.addEventListener('show', function(event) {
   var acc = document.getElementsByClassName('collapsible-header');
+  var lastActive;
 
   for (var i = 0; i < acc.length; ++i) {
     acc[i].onmousedown = function() {
-      this.classList.toggle("active");
+      if (lastActive !== undefined && lastActive !== this) {
+        lastActive.classList.toggle('active');
+        lastActive.nextElementSibling.style.maxHeight = null;
+        lastActive.nextElementSibling.style.borderBottomWidth = '0';
+      }
+
+      this.classList.toggle('active');
 
       /* Set max-height to animate the dropdown
       Source: https://www.w3schools.com/howto/howto_js_accordion.asp */
@@ -15,6 +22,7 @@ document.addEventListener('show', function(event) {
         panel.style.maxHeight = panel.scrollHeight + 'px';
         panel.style.borderBottomWidth = '1px';
       }
+      lastActive = this;
     };
   }
 });
