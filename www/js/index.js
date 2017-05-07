7c8d0beb2965f3e75583adc1c07040ee3efdf9d0
Lev <leva181777@gmail.com>
2017-05-07 20:08:27 +0000

    :lipstick: Adds Compare section and beginnings of About

--- www/js/index.js
+++ www/js/index.js
@@ -1,51 +1,182 @@
+// How many chips to allow
+var chipThreshold = 5;
+
+function genSubjects(cls) {
+  /**
+   * Generates random subjects for a given class.
+   * @param  {String} cls   Class name.
+   * @return {Array}        An array of 7 subjects.
+   */
+  var subjects = ['Математика', 'Информатика', 'Физика', 'Обществознание', 'Русский язык', 'Литература', 'Нет урока'];
+  var selected = [];
+  for (var i = 0; i < 7; ++i) {
+    selected.push(subjects[Math.floor(Math.random() * subjects.length)]);
+  }
+  return selected;
+}
+
 document.addEventListener('show', function(event) {
+  // Set up card-reveal
   var act = document.getElementsByClassName('activator');
 
-  for (var i = 0; i < act.length; ++i) {
-    act[i].onmousedown = function() {
-      var reveal = this.parentElement.nextElementSibling;
-      if (reveal.style.height) {
-        reveal.style.height = null;
-      } else {
-        reveal.style.height = '100%';
-      }
-    };
-
-    var closeBt = act[i].parentElement.nextElementSibling.getElementsByClassName('card-title')[0].getElementsByTagName('i')[0];
-    closeBt.onmousedown = function() {
-      var reveal = this.parentElement.parentElement.parentElement.parentElement;
-      if (reveal.style.height) {
-        reveal.style.height = null;
-      } else {
-        reveal.style.height = '100%';
-      }
-    };
+  function revealCard() {
+    // Display the card-reveal with a slide animation
+    var reveal = this.parentElement.nextElementSibling;
+    reveal.style.height = '100%';
   }
 
+  function hideCard() {
+    // Hide the card-reveal with a slide animation
+    var reveal = this.parentElement.parentElement.parentElement.parentElement;
+    reveal.style.height = null;
+  }
+
+  for (var i = 0; i < act.length; ++i) {
+    act[i].onmousedown = revealCard;
+
+    var closeBt = document.getElementById('reveal-close');
+    closeBt.onmousedown = hideCard;
+  }
+
+  function removeChip() {
+    // Action to take on a chip's removal
+    var idx = usedTags.indexOf(this.tag);
+
+    if (usedTags.length === chipThreshold) {
+      // If the limit is not exceeded, allow selection
+      this.dropdownButton.removeAttribute('disabled');
+    }
+
+    // Delete the corresponding row from the table
+    var rowHead = document.querySelector('thead th:nth-child(' + (idx + 2) + ')');
+    var rowBody = document.querySelectorAll('tbody td:nth-child(' + (idx + 2) + ')');
+    rowHead.parentElement.removeChild(rowHead);
+    for (var i = 0; i < rowBody.length; ++i) {
+      rowBody[i].parentElement.removeChild(rowBody[i]);
+    }
+
+    usedTags.splice(idx, 1);
+    this.parentElement.removeChild(this);
+    this.selectButton.removeAttribute('disabled');
+
+    // If there's no more chips, hide the table
+    if (!usedTags.length) {
+      document.getElementById('comparison-table').classList.add('hide');
+      document.querySelector('div.row.empty').classList.remove('hide');
+    }
+
+  }
+
+  // Set up dropdowns with selectable content
   var drop = document.getElementsByClassName('dropdown-content');
+  var usedTags = [];
   for (var i = 0; i < drop.length; ++i) {
     drop[i].style.width = drop[i].previousElementSibling.offsetWidth + 'px';
+    drop[i].style.top = drop[i].previousElementSibling.offsetTop + 'px';
     drop[i].previousElementSibling.onmousedown = function() {
       this.classList.toggle('active');
     };
 
     var bts = drop[i].getElementsByTagName('ons-button');
-    for (var j = 0; j < bts.length; ++j) {
-      bts[j].dropdownButton = drop[i].previousElementSibling;
-      bts[j].onmousedown = function() {
+    if (drop[i].matches('.set-text')) {
+      function dropAction() {
+        // Simply replace the button's text with the selected item's
         this.dropdownButton.innerHTML = this.innerHTML;
         this.dropdownButton.classList.toggle('active');
-      };
+      }
+    }
+    else {
+      function dropAction() {
+        // Add a chip
+        var chips = this.dropdownButton.parentElement.nextElementSibling;
+
+        var newChip = document.createElement('div');
+        // Save the following objects:
+        newChip.selectButton = this;  // the button that was used to select the chip;
+        newChip.tag = this.textContent;  // the chip's tag;
+        newChip.dropdownButton = this.dropdownButton;  // the button that triggers the dropdown
+
+        // Set up the chip node
+        newChip.classList.add('chip');
+        newChip.innerHTML = this.textContent + '<i class="material-icons close">close</i>';
+        newChip.onmousedown = removeChip;
+
+        chips.appendChild(newChip);
+        usedTags.push(newChip.tag);
+
+        this.dropdownButton.classList.toggle('active');
+        this.setAttribute('disabled', 'true');
+
+        if (usedTags.length === chipThreshold) {
+          this.dropdownButton.setAttribute('disabled', 'true');
+        }
+
+        // If there's a chip, show the table
+        if (usedTags.length === 1) {
+          document.getElementById('comparison-table').classList.remove('hide');
+          document.getElementsByClassName('empty')[0].classList.add('hide');
+        }
+
+        // Add the class to the table
+        var tableHeader = document.querySelector('#comparison-table thead tr');
+        var newHeading = document.createElement('th');
+        newHeading.innerHTML = newChip.tag;
+        tableHeader.appendChild(newHeading);
+
+        var subjects = genSubjects(newChip.tag);
+        var tableRows = document.querySelectorAll('#comparison-table tbody tr');
+        for (var i = 0; i < tableRows.length; ++i) {
+          var tableCell = document.createElement('td');
+          tableCell.innerHTML = subjects[i];
+          tableRows[i].appendChild(tableCell);
+        }
+      }
+    }
+
+    for (var j = 0; j < bts.length; ++j) {
+      bts[j].dropdownButton = drop[i].previousElementSibling;
+      bts[j].onmousedown = dropAction;
     }
   }
 
   if (drop.length) {
+    // If there's a dropdown, listen for clicks outside it to close it
+    var allowed = '.dropdown-act, .dropdown-content li ons-button, .collapsible-header';
     window.onmousedown = function(event) {
-      if (!event.target.matches('.dropdown-content, .dropdown-act, .dropdown-content li ons-button, .collapsible-header')) {
+      if (!event.target.matches(allowed)) {
         for (var i = 0; i < drop.length; ++i) {
           drop[i].previousElementSibling.classList.remove('active');
         }
       }
     };
   }
+
+  // Prevent SideNav from opening when the table gets scrolled
+  var isTouchDown = false;
+  var table = document.getElementById('comparison-table');
+  table.ontouchstart = function() { isTouchDown = true  };
+  table.ontouchend = function() { isTouchDown = false };
+  table.ontouchmove = function(event) {
+    if (isTouchDown) {
+      event.stopPropagation();
+    }
+  };
+
+  var isMouseDown = false;
+  table.onmousedown = function(event) {
+    event.preventDefault();
+    isMouseDown = true;
+  };
+  table.onmouseup = function() {
+    event.preventDefault();
+    isMouseDown = false;
+  };
+  table.onmousemove = function(event) {
+    event.preventDefault();
+    if (isMouseDown) {
+      event.stopPropagation();
+    }
+  };
+
 });
+
