bd1bef529e44ee14b4d3b1f08bf8cdd9675a30ec
Lev <leva181777@gmail.com>
2017-06-09 17:28:51 +0000

    :ambulance: Adds logic to avoid databases in Firefox

--- www/js/db.js
+++ www/js/db.js
@@ -3,24 +3,30 @@ function preparePages() {
   db.transaction(function (tx) {
     // Insert the user's name and class into the menu
     tx.executeSql('SELECT * FROM settings WHERE key="name"', [], function (tx, results) {
-      document.getElementById('#menu-user-name').textContent = results.rows.item(0).value;
+      document.getElementById('menu-user-name').textContent = results.rows.item(0).value;
     });
     tx.executeSql('SELECT * FROM settings WHERE key="class"', [], function (tx, results) {
-      document.getElementById('#menu-user-class').textContent = results.rows.item(0).value;
+      document.getElementById('menu-user-class').textContent = results.rows.item(0).value;
     });
   });
 };
 
-db.transaction(function (tx) {
-  tx.executeSql('SELECT * FROM settings WHERE key="class"', [], function (tx, results) {
-    // If it is the first launch, navigate user to the Intro
-    if (!results.rows.length) {
-      fn.load('intro.html');
-    }
-    // Otherwise, fill in the data and redirect to Home
-    else {
-      preparePages();
-      fn.load('home.html');
-    }
-  }, null);
-});
+// If Firefox, redirect to home straight away
+if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
+  fn.load('home.html');
+}
+else {
+  db.transaction(function (tx) {
+    tx.executeSql('SELECT * FROM settings WHERE key="class"', [], function (tx, results) {
+      // If it is the first launch, navigate user to the Intro
+      if (!results.rows.length) {
+        fn.load('intro.html');
+      }
+      // Otherwise, fill in the data and redirect to Home
+      else {
+        preparePages();
+        fn.load('home.html');
+      }
+    }, null);
+  });
+}
