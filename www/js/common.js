ed1ee3dc8890b6aacfa0213f84035b7891732f72
Lev <leva181777@gmail.com>
2017-06-09 17:28:51 +0000

    :ambulance: Adds logic to avoid databases in Firefox

--- www/js/common.js
+++ www/js/common.js
@@ -12,25 +12,33 @@ window.fn.load = function(page) {
   navi.resetToPage(page, {animation: 'none'});
 };
 
-// Open the database
-var db = openDatabase('timetable', '1.0', 'Main app data storage', 2 * 1024 * 1024);
+// If Firefox, define a dummy function
+if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
+  function checkIntroFields() {
+    fn.load('home.html');
+  }
+}
+else {
+  // Open the database
+  var db = openDatabase('timetable', '1.0', 'Main app data storage', 2 * 1024 * 1024);
 
-// Set up the schema
-db.transaction(function (tx) {
-  tx.executeSql('CREATE TABLE IF NOT EXISTS settings (key TEXT, value TEXT)', []);
-});
+  // Set up the schema
+  db.transaction(function (tx) {
+    tx.executeSql('CREATE TABLE IF NOT EXISTS settings (key TEXT, value TEXT)', []);
+  });
 
-// Init OneSignal
-document.addEventListener('deviceready', function () {
-  // What happens when notification is received
-  var notificationOpenedCallback = function(jsonData) {
-    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
-  };
+  // Init OneSignal
+  document.addEventListener('deviceready', function () {
+    // What happens when notification is received
+    var notificationOpenedCallback = function(jsonData) {
+      console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+    };
 
-  window.plugins.OneSignal
-    // It is okay to keep the key here since this account is only for testing purposes
-    .startInit("1a83588c-f4b3-40b5-9843-2fc6423f6186")
-    // Define where should we work with received notifications
-    .handleNotificationOpened(notificationOpenedCallback)
-    .endInit();
-}, false);
+    window.plugins.OneSignal
+      // It is okay to keep the key here since this account is only for testing purposes
+      .startInit("1a83588c-f4b3-40b5-9843-2fc6423f6186")
+      // Define where should we work with received notifications
+      .handleNotificationOpened(notificationOpenedCallback)
+      .endInit();
+  }, false);
+}
